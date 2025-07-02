8cf2c4bebfa134e480069b39e7404883a23467a3
jieun <amykim0116@naver.com>
2025-07-02 16:49:57 +0900

    alphabet comapre

--- Project10/similarity_check.cpp
+++ Project10/similarity_check.cpp
@@ -1,6 +1,10 @@
 #include<string>
 
 using std::string;
+struct IsAlphabetTouched {
+	bool input1 = false;
+	bool input2 = false;
+};
 
 class SimilarityChecker {
 public:
@@ -19,9 +23,48 @@ public:
 		return (1 - gap) * MAX_LENGTH_POINT;
 	}
 
+	double getAlphabetPoint(string input1, string input2) {
+
+		for (char ch : input1)
+		{
+			if (ch <ALPHABET_START || ch > ALPHABET_END) continue;
+			int idx = ch - ALPHABET_START;
+			alphabetTouchArr[idx].input1 = true;
+		}
+
+		for (char ch : input2)
+		{
+			if (ch <ALPHABET_START || ch > ALPHABET_END) continue;
+			int idx = ch - ALPHABET_START;
+			alphabetTouchArr[idx].input2 = true;
+		}
+
+		double point = 0;
+		double total = 0;
+
+		for (int idx = 0; idx < ALPHABET_COUNT; idx++)
+		{
+			if (alphabetTouchArr[idx].input1 && alphabetTouchArr[idx].input2) {
+				point += 1;
+			}
+			if (alphabetTouchArr[idx].input1 || alphabetTouchArr[idx].input2) {
+				total += 1;
+			}
+		}
+		if (total == 0) return 0;
+
+		return (point / total) * MAX_ALPHABET_POINT;
+	}
+
 
 private:
 	static const int MAX_LENGTH_POINT = 60;
+	static const int MAX_ALPHABET_POINT = 40;
+	static const int ALPHABET_START = 'A';
+	static const  int ALPHABET_END = 'Z';
+	static const int ALPHABET_COUNT = 'Z' - 'A' + 1;
+
+	IsAlphabetTouched alphabetTouchArr[ALPHABET_COUNT];
 
 	bool IsLengthZeroPoint(int length1, int length2)
 	{
--- Project10/similarity_check_test.cpp
+++ Project10/similarity_check_test.cpp
@@ -11,20 +11,32 @@ private:
 	SimilarityChecker similarityChecker;
 
 public:
-	void checkSimilarity(string input1, string input2, int expected)
+	void checkLength(string input1, string input2, int expected)
 	{
 		EXPECT_EQ(expected, similarityChecker.getLengthPoint(input1, input2));
+	}
+	void checkAlphabet(string input1, string input2, int expected)
+	{
+		EXPECT_EQ(expected, similarityChecker.getAlphabetPoint(input1, input2));
 	}
 };
 
 TEST_F(SimilarityCheckerFixture, LengthCheckMaxPoint) {
-	checkSimilarity("ABDC", "ABCD", 60);
+	checkLength("ABDC", "ABCD", 60);
 }
 
 TEST_F(SimilarityCheckerFixture, LengthCheckZeroPoint) {
-	checkSimilarity("ABDC", "ABCDABCDdfsdsfs", 0);
+	checkLength("ABDC", "ABCDABCDdfsdsfs", 0);
 }
 
 TEST_F(SimilarityCheckerFixture, LengthCheckPartialPoint) {
-	checkSimilarity("ABCDEFGHIJ", "ABCDEFGHIJwoej", 36);
+	checkLength("ABCDEFGHIJ", "ABCDEFGHIJwoej", 36);
+}
+
+TEST_F(SimilarityCheckerFixture, AlphabetCheckMaxPoint) {
+	checkAlphabet("ABDC", "ABCD", 40);
+}
+
+TEST_F(SimilarityCheckerFixture, AlphabetCheckZeroPoint) {
+	checkAlphabet("ABDC", "QWER", 0);
 }
