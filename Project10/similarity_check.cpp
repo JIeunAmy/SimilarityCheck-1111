9fb81a021f9ca892f4864c9e14201372529aacc9
jieun <amykim0116@naver.com>
2025-07-02 15:38:03 +0900

    1 refac

--- Project10/similarity_check.cpp
+++ Project10/similarity_check.cpp
@@ -5,6 +5,9 @@ using std::string;
 class SimilarityChecker {
 public:
 	int getLengthPoint(string input1, string input2) {
-		return 60;
+		return MAX_LENGTH_POINT;
 	}
+
+private:
+	static const int MAX_LENGTH_POINT = 60;
 };
