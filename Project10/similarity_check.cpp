d16b32152dff231eef7089910cdfc4cc31538188
jieun <amykim0116@naver.com>
2025-07-02 15:40:16 +0900

    2 green

--- Project10/similarity_check.cpp
+++ Project10/similarity_check.cpp
@@ -5,7 +5,11 @@ using std::string;
 class SimilarityChecker {
 public:
 	int getLengthPoint(string input1, string input2) {
-		return MAX_LENGTH_POINT;
+		int size1 = input1.size();
+		int size2 = input2.size();
+
+		if (size1 == size2) { return MAX_LENGTH_POINT; }
+		if ((size1 * 2 == size2) || (size2 * 2 == size1)) { return 0; }
 	}
 
 private:
--- Project10/similarity_check_test.cpp
+++ Project10/similarity_check_test.cpp
@@ -5,12 +5,22 @@
 using namespace std;
 using namespace testing;
 
-TEST(SimilarStingTest, LengthCheck) {
+TEST(SimilarStingTest, LengthCheckMaxPoint) {
 	SimilarityChecker similarityChecker;
 
 	string input1 = "ABCD";
 	string input2 = "ABCD";
 	int expected = 60;
+
+	EXPECT_EQ(expected, similarityChecker.getLengthPoint(input1, input2));
+}
+
+TEST(SimilarStingTest, LengthCheckZeroPoint) {
+	SimilarityChecker similarityChecker;
+
+	string input1 = "ABCD";
+	string input2 = "ABCDABCD";
+	int expected = 0;
 
 	EXPECT_EQ(expected, similarityChecker.getLengthPoint(input1, input2));
 }
